826bbbbca8d61cf3afe20671b05b8ca594d54450
Tony Shen <fengzhizi715@126.com>
2024-07-26 11:01:02 +0800

    Update library.h

--- library.h
+++ library.h
@@ -12,18 +12,39 @@
 using namespace std;
 using namespace cv;
 
+/**
+ * 该算法库的版本号
+ */
 string getVersion();
 
+/**
+ * 当前使用的 OpenCV 的版本号
+ */
 string getOpenCVVersion();
 
+/**
+ * 直方图均衡化
+ */
 Mat equalizeHistImage(Mat src);
 
+/**
+ * gamma 校正
+ */
 void gammaCorrection(Mat& src, Mat& dst, float K);
 
+/**
+ * laplace 锐化，主要是 8 邻域卷积核
+ */
 void laplace(Mat& src, Mat& dst);
 
+/**
+ * USM 锐化
+ */
 void unsharpMask(const Mat& src, Mat& dst, int radius, int threshold, int amount);
 
+/**
+ * 自动色彩均衡
+ */
 void ace(Mat& src, Mat& dst, int ratio, int radius);
 
 #endif //MONICAIMAGEPROCESS_LIBRARY_H
