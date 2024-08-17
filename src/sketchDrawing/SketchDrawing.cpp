82377e7889281a28ee8575c0766d4de6d0040809
Tony Shen <fengzhizi715@126.com>
2024-08-17 23:55:22 +0800

    Update SketchDrawing.cpp

--- src/sketchDrawing/SketchDrawing.cpp
+++ src/sketchDrawing/SketchDrawing.cpp
@@ -15,7 +15,11 @@ SketchDrawing::SketchDrawing(string modelPath, const char* logId, const char* pr
 
 Mat SketchDrawing::detect(Mat& image)
 {
+    double preprocessTime = 0.0;
     double inferenceTime = 0.0;
+    double postprocessTime = 0.0;
+
+    Timer preprocessTimer = Timer(preprocessTime, true);
     Mat dst;
     resize(image, dst, Size(this->inpWidth, this->inpHeight));
     this->input_image_.resize(this->inpWidth * this->inpHeight * dst.channels());
@@ -34,18 +38,26 @@ Mat SketchDrawing::detect(Mat& image)
 
     auto allocator_info = MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
     Value input_tensor_ = Value::CreateTensor<float>(allocator_info, input_image_.data(), input_image_.size(), input_shape_.data(), input_shape_.size());
+    preprocessTimer.Stop();
 
     Timer inferenceTimer = Timer(inferenceTime, true);
     vector<Value> ort_outputs = this -> forward(input_tensor_);
     inferenceTimer.Stop();
 
-    std::cout << (inferenceTime * 1000.0) << "ms inference" << std::endl;
+    Timer postprocessTimer = Timer(postprocessTime, true);
 
     float* pred = ort_outputs[0].GetTensorMutableData<float>();
     Mat result(outHeight, outWidth, CV_32FC1, pred);
     result *= 255;
     result.convertTo(result, CV_8UC1);
     resize(result, result, Size(image.cols, image.rows));
+    postprocessTimer.Stop();
+
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << "image: " << image.rows << "x" << image.cols <<" "<< (preprocessTime + inferenceTime + postprocessTime) * 1000.0 << "ms" << std::endl;
+    std::cout << "speed: " << (preprocessTime * 1000.0) << "ms preprocess, ";
+    std::cout << (inferenceTime * 1000.0) << "ms inference, ";
+    std::cout << (postprocessTime * 1000.0) << "ms postprocess per image " << std::endl;
 
     return result;
 }
