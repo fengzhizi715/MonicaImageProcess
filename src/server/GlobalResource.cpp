2aadce1d5628b3778d22c13413734480e29cb72e
Tony Shen <fengzhizi715@126.com>
2025-03-25 17:49:10 +0800

    update

--- src/server/GlobalResource.cpp
+++ src/server/GlobalResource.cpp
@@ -3,8 +3,15 @@
 //
 #include <iostream>
 #include "GlobalResource.h"
+#include "../../include/Constants.h"
 
 GlobalResource::GlobalResource(string modelPath): modelPath(modelPath) {
+
+    string sketchDrawingMode = modelPath + "/opensketch_style_512x512.onnx";
+    const string& onnx_logid = "Sketch Drawing";
+    const string& onnx_provider = OnnxProviders::CPU;
+    sketchDrawing = std::make_unique<SketchDrawing>(sketchDrawingMode, onnx_logid.c_str(), onnx_provider.c_str());
+
     // 初始化资源，比如加载模型、配置文件等
     std::cout << "GlobalResource initialized." << std::endl;
 }
--- src/server/GlobalResource.h
+++ src/server/GlobalResource.h
@@ -5,16 +5,21 @@
 #ifndef MONICAIMAGEPROCESS_GLOBALRESOURCE_H
 #define MONICAIMAGEPROCESS_GLOBALRESOURCE_H
 
+#include <memory>
+#include "../../include/sketchDrawing/SketchDrawing.h"
+
 using namespace std;
 
 class GlobalResource {
 public:
     GlobalResource(string modelPath);
     void processImage();
-    void initSketchDrawingModel();
 
 private:
     string modelPath;
+
+    // 使用智能指针管理SketchDrawing对象
+    std::unique_ptr<SketchDrawing> sketchDrawing;
 };
 
 #endif //MONICAIMAGEPROCESS_GLOBALRESOURCE_H
