528ba659e963d6253e8bbb357694354b5b2792a0
Tony Shen <fengzhizi715@126.com>
2025-07-16 17:54:31 +0800

    refactor

--- src/jni/cn_netdiscovery_monica_opencv_ImageProcess.cpp
+++ src/jni/cn_netdiscovery_monica_opencv_ImageProcess.cpp
@@ -1,8 +1,6 @@
 //
 // Created by Tony Shen on 2024/7/14.
 //
-#include <libraw/libraw.h>
-#include <libheif/heif.h>
 #include <vector>
 #include <cstring>
 #include <iostream>
@@ -13,12 +11,13 @@
 #include "../../include/utils/Utils.h"
 #include "utils/jni_utils.h"
 #include "color_correction/color_correction_internal.h"
+#include "format_decoder/format_decoder_internal.h"
 
 MatchTemplate   *match_template = nullptr;
 
 JNIEXPORT jstring JNICALL Java_cn_netdiscovery_monica_opencv_ImageProcess_getVersion
         (JNIEnv* env, jobject) {
-    string version = "v0.2.2";
+    string version = "v0.2.3";
     return env->NewStringUTF(version.c_str());
 }
 
@@ -449,119 +448,10 @@ JNIEXPORT jintArray JNICALL Java_cn_netdiscovery_monica_opencv_ImageProcess_matc
 
 JNIEXPORT jobject JNICALL Java_cn_netdiscovery_monica_opencv_ImageProcess_decodeRawToBuffer
         (JNIEnv *env, jobject obj, jstring filePath) {
-    const char *path = env->GetStringUTFChars(filePath, nullptr);
-
-    LibRaw rawProcessor;
-    if (rawProcessor.open_file(path) != LIBRAW_SUCCESS || rawProcessor.unpack() != LIBRAW_SUCCESS) {
-        env->ReleaseStringUTFChars(filePath, path);
-        return nullptr;
-    }
-
-    rawProcessor.imgdata.params.output_bps = 8;
-    rawProcessor.imgdata.params.use_camera_wb = 1;
-    rawProcessor.imgdata.params.no_auto_bright = 1;
-    rawProcessor.dcraw_process();
-
-    libraw_processed_image_t *img = rawProcessor.dcraw_make_mem_image();
-
-    if (!img || img->type != LIBRAW_IMAGE_BITMAP) {
-        rawProcessor.recycle();
-        env->ReleaseStringUTFChars(filePath, path);
-        return nullptr;
-    }
-
-    // 假设 img->colors == 3（RGB）
-    int width = img->width;
-    int height = img->height;
-    int channels = img->colors;
-    int length = width * height * channels;
-
-    jbyteArray byteArray = env->NewByteArray(length);
-    env->SetByteArrayRegion(byteArray, 0, length, reinterpret_cast<jbyte *>(img->data));
-
-    jclass bufferCls = env->FindClass("cn/netdiscovery/monica/domain/RawImage");
-    jmethodID constructor = env->GetMethodID(bufferCls, "<init>", "([BIII)V");
-    jobject result = env->NewObject(bufferCls, constructor, byteArray, width, height, channels);
-
-    LibRaw::dcraw_clear_mem(img);
-    rawProcessor.recycle();
-    env->ReleaseStringUTFChars(filePath, path);
-    return result;
+      return decodeRawToBufferInternal(env, filePath);
 }
 
 JNIEXPORT jobject JNICALL Java_cn_netdiscovery_monica_opencv_ImageProcess_decodeHeif
         (JNIEnv *env, jobject obj, jstring filePath) {
-    const char *cpath = env->GetStringUTFChars(filePath, nullptr);
-
-    heif_context* ctx = heif_context_alloc();
-    heif_error err = heif_context_read_from_file(ctx, cpath, nullptr);
-    if (err.code != heif_error_Ok) {
-        std::cerr << "Failed to read HEIF: " << err.message << std::endl;
-        heif_context_free(ctx);
-        env->ReleaseStringUTFChars(filePath, cpath);
-        return nullptr;
-    }
-
-    heif_image_handle* handle = nullptr;
-    err = heif_context_get_primary_image_handle(ctx, &handle);
-    if (err.code != heif_error_Ok) {
-        std::cerr << "Failed to get primary image handle" << std::endl;
-        heif_context_free(ctx);
-        env->ReleaseStringUTFChars(filePath, cpath);
-        return nullptr;
-    }
-
-    heif_image* img = nullptr;
-    err = heif_decode_image(handle, &img, heif_colorspace_RGB, heif_chroma_interleaved_RGBA, nullptr);
-    if (err.code != heif_error_Ok) {
-        std::cerr << "Failed to decode image" << std::endl;
-        heif_image_handle_release(handle);
-        heif_context_free(ctx);
-        env->ReleaseStringUTFChars(filePath, cpath);
-        return nullptr;
-    }
-
-    int width = heif_image_get_width(img, heif_channel_interleaved);
-    int height = heif_image_get_height(img, heif_channel_interleaved);
-
-    int stride = 0;
-    const uint8_t* data = heif_image_get_plane_readonly(img, heif_channel_interleaved, &stride);
-    if (!data) {
-        std::cerr << "Failed to get pixel data" << std::endl;
-        heif_image_release(img);
-        heif_image_handle_release(handle);
-        heif_context_free(ctx);
-        env->ReleaseStringUTFChars(filePath, cpath);
-        return nullptr;
-    }
-
-    // 创建 Java int[] pixels
-    jintArray pixelArray = env->NewIntArray(width * height);
-    jint* pixels = env->GetIntArrayElements(pixelArray, nullptr);
-
-    for (int y = 0; y < height; ++y) {
-        const uint8_t* row = data + y * stride;
-        for (int x = 0; x < width; ++x) {
-            uint8_t r = row[x * 4 + 0];
-            uint8_t g = row[x * 4 + 1];
-            uint8_t b = row[x * 4 + 2];
-            uint8_t a = row[x * 4 + 3];
-            pixels[y * width + x] = ((a << 24) | (r << 16) | (g << 8) | b);
-        }
-    }
-
-    env->ReleaseIntArrayElements(pixelArray, pixels, 0);
-
-    // 构建 HeifImage Java 对象
-    jclass heifImageCls = env->FindClass("cn/netdiscovery/monica/domain/HeifImage");
-    jmethodID ctor = env->GetMethodID(heifImageCls, "<init>", "(II[I)V");
-    jobject heifImage = env->NewObject(heifImageCls, ctor, width, height, pixelArray);
-
-    // 清理
-    heif_image_release(img);
-    heif_image_handle_release(handle);
-    heif_context_free(ctx);
-    env->ReleaseStringUTFChars(filePath, cpath);
-
-    return heifImage;
+    return decodeHeifInternal(env, filePath);
 }
---
+++ src/jni/format_decoder/format_decoder_internal.h
@@ -0,0 +1,15 @@
+//
+// Created by Tony on 2025/7/16.
+//
+
+#ifndef MONICAIMAGEPROCESS_FORMAT_DECODER_INTERNAL_H
+#define MONICAIMAGEPROCESS_FORMAT_DECODER_INTERNAL_H
+
+#pragma once
+#include <jni.h>
+
+jobject decodeRawToBufferInternal(JNIEnv *env, jstring filePath);
+
+jobject decodeHeifInternal(JNIEnv *env, jstring filePath);
+
+#endif //MONICAIMAGEPROCESS_FORMAT_DECODER_INTERNAL_H
